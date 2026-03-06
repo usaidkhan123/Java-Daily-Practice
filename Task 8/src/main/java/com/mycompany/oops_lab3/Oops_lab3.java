610e7ac56c73609a967c180f650a95486678d695
Muhammad Usaid Khan <161123814+usaidkhan123@users.noreply.github.com>
2026-03-06 23:18:53 +0500

    Add files via upload

---
+++ Task 8/src/main/java/com/mycompany/oops_lab3/Oops_lab3.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oops_lab3;
+
+public class Oops_lab3 {
+
+    public static void main(String[] args) {
+        
+    }
+}
