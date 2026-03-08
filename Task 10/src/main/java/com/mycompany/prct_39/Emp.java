b5675e341c6ec75b87547828ea5471a33516fd3f
Muhammad Usaid Khan <161123814+usaidkhan123@users.noreply.github.com>
2026-03-08 22:00:54 +0500

    Add files via upload

---
+++ Task 10/src/main/java/com/mycompany/prct_39/Emp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prct_39;
+
+/**
+ *
+ * @author User
+ */
+public class Emp {
+    int salary;
+    String name;
+    public int getSalary(){
+        return salary;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        name = n;
+    }
+}
---
+++ Task 10/src/main/java/com/mycompany/prct_39/Prct_39.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prct_39;
+
+/**
+ *
+ * @author User
+ */
+public class Prct_39 {
+
+    public static void main(String[] args) {
+        Emp emp1 = new Emp();
+        emp1.name = "Muhamamd Usaid Khan";
+        emp1.salary = 87150;
+        emp1.setName("Sohaib");
+        System.out.println(emp1.getSalary());
+        System.out.println(emp1.getName());
+        
+    }
+}
