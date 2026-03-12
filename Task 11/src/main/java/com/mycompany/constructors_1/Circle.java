8d15c98e2aad729ba37c93091d220e6949b097dd
Muhammad Usaid Khan <161123814+usaidkhan123@users.noreply.github.com>
2026-03-13 01:54:45 +0500

    Add files via upload

---
+++ Task 11/src/main/java/com/mycompany/constructors_1/Circle.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.constructors_1;
+
+public class Circle {
+    private double cirPara;
+    private double cirRad;
+    private double cirAr;
+    
+    public double getPara(){
+        return cirPara;
+    }
+    
+    public double getAr(){
+        return cirAr;
+    }
+    
+    public double getRad(){
+        return cirRad;
+    }
+    
+    public void setPara( double p){
+        cirPara = p;        
+    }
+    
+    public void setRad( double r){
+        cirRad = r;
+    }
+    
+    public void setAr( double a){
+        cirAr = a;
+    }
+}
---
+++ Task 11/src/main/java/com/mycompany/constructors_1/Constructors_1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.constructors_1;
+
+/**
+ *
+ * @author User
+ */
+public class Constructors_1 {
+
+    public static void main(String[] args) {
+      Circle cir = new Circle();
+      cir.setAr(10.3);
+      System.out.println(cir.getAr());
+      cir.setPara(12.5);
+      System.out.println(cir.getPara());
+      cir.setRad(98.2);
+      System.out.println(cir.getRad());
+    }
+}
